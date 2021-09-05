d76cfd4339fd970a7dbbd30192f674e0d58cb5c6
burzomir <burzomirdev@gmail.com>
2021-09-05 20:49:04 +0200

    Add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
 import { ReactComponent as Pattern1 } from "./patterns/pattern-1.svg";
 import { ReactComponent as Pattern2 } from "./patterns/pattern-2.svg";
 import { Pattern } from "./patterns/Pattern";
+import { Container, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+  },
+}));
 
 function App() {
+  const styles = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Pattern PatternSource={Pattern1} colors={["red", "green", "blue"]} />
-      <Pattern PatternSource={Pattern2} colors={["cornflowerblue"]} />
-    </div>
+    <Container className={styles.root}>
+      <Paper>
+        <Pattern PatternSource={Pattern1} colors={["red", "green", "blue"]} />
+        <Pattern PatternSource={Pattern2} colors={["cornflowerblue"]} />
+      </Paper>
+    </Container>
   );
 }
 
