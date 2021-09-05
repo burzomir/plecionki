d7663e4b3466987f0e8dc799f47f6cc4f21a4e5c
burzomir <burzomirdev@gmail.com>
2021-09-05 21:16:05 +0200

    Implement creator model

---
+++ src/Creator.ts
@@ -0,0 +1,69 @@
+type PatternSelectionStep = {
+  step: "Pattern Selection";
+};
+
+type ColorSelectionStep = {
+  step: "Color Selection";
+  pattern: string;
+};
+
+type PersonalInformationStep = {
+  step: "Personal Information";
+  pattern: string;
+  colors: string[];
+};
+
+type SummaryStep = {
+  step: "Summary";
+  pattern: string;
+  colors: string[];
+  name: string;
+  email: string;
+};
+
+export type Creator =
+  | PatternSelectionStep
+  | ColorSelectionStep
+  | PersonalInformationStep
+  | SummaryStep;
+
+export function start(): Creator {
+  return {
+    step: "Pattern Selection",
+  };
+}
+
+export function selectPattern(
+  pattern: string,
+  _: PatternSelectionStep
+): Creator {
+  return {
+    step: "Color Selection",
+    pattern,
+  };
+}
+
+export function selectColors(
+  colors: string[],
+  { pattern }: ColorSelectionStep
+): Creator {
+  return {
+    step: "Personal Information",
+    pattern,
+    colors,
+  };
+}
+
+export function providePersonalInformation(
+  name: string,
+  email: string,
+  { pattern, colors }: PersonalInformationStep
+): Creator {
+  return {
+    step: "Summary",
+    pattern,
+    colors,
+    name,
+    email,
+  };
+}
