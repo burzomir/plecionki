3097719ed8c34a32c7e00ba554b48cd706fbe7c7
burzomir <burzomirdev@gmail.com>
2021-09-05 23:16:41 +0200

    Color picker prototype

--- src/steps/ColorSelection.tsx
+++ src/steps/ColorSelection.tsx
@@ -1,7 +1,11 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { IconButton, makeStyles, Popover, Typography } from "@material-ui/core";
 import { ColorSelectionStep, Creator } from "../Creator";
 import { Patterns, PatternsMap } from "../patterns/Patterns";
 import { Pattern } from "../patterns/Pattern";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import { useState } from "react";
+import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 
 type ColorSelectionProps = {
   creator: ColorSelectionStep;
@@ -39,6 +43,64 @@ export function ColorSelection({ creator, onSelect }: ColorSelectionProps) {
           colors={[]}
         />
       </div>
+      <Color value="red" />
+      <Color value="green" />
+      <Color value="new" />
+    </>
+  );
+}
+
+type ColorProps = {
+  value: string;
+};
+
+function Color({ value }: ColorProps) {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  return (
+    <>
+      {value === "new" && (
+        <IconButton
+          style={{ fontSize: "2.5rem" }}
+          onClick={(e) => setAnchorEl(e.currentTarget)}
+          title="Add color"
+        >
+          <AddCircleIcon style={{ fontSize: "2.5rem" }} />
+        </IconButton>
+      )}
+      {value !== "new" && (
+        <IconButton
+          style={{ color: value, fontSize: "3rem" }}
+          onClick={(e) => setAnchorEl(e.currentTarget)}
+        >
+          <FiberManualRecordIcon style={{ fontSize: "3rem" }} />
+        </IconButton>
+      )}
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <IconButton style={{ color: "red", fontSize: "3rem" }}>
+          <FiberManualRecordIcon style={{ fontSize: "3rem" }} />
+        </IconButton>
+        <IconButton style={{ color: "green", fontSize: "3rem" }}>
+          <FiberManualRecordIcon style={{ fontSize: "3rem" }} />
+        </IconButton>
+        {value !== "new" && (
+          <IconButton style={{ fontSize: "2.5rem" }} title="Remove color">
+            <RemoveCircleIcon style={{ fontSize: "2.5rem" }} />
+          </IconButton>
+        )}
+      </Popover>
     </>
   );
 }
